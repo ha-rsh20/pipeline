ac91573620916e5da23f19e34b7521088ea4be01
ha-rsh20 <20it033@charusat.edu.in>
2023-03-14 22:03:47 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, from 20it033 - Harshkumar Gajjar";
+    }
+
+}
